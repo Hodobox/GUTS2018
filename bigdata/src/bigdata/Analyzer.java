019f97a4c8a267f9211c6a8146523468a792aa46
iteriter <44101915+iteriter@users.noreply.github.com>
2018-10-13 16:56:23 +0100

    update WorldMap package; update Analyzer

--- bigdata/src/bigdata/Analyzer.java
+++ bigdata/src/bigdata/Analyzer.java
@@ -1,31 +1,51 @@
 package bigdata;
 
+import bigdata.WorldMap.MapAggregation;
+import bigdata.WorldMap.WorldMapData;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Analyzer {
 
     private static final int DEFAULT_GRID_SIZE = 16;
     private static final String[] FIELDS_OF_INTEREST = {"age", "gender", "drg", "price"};
 
-    /* process list of data entries pre-filtered by parser into data structure
-       usable by map plotter */
-    public void processData(ArrayList<Record> entries, String format) {
-        String mapData;
+    private MapAggregation aggregationFormat;
+    private ArrayList<String> mapData = new ArrayList<String>();
 
-        switch(format){
-            case "state":
-                // Get list of us states with codes
-                // String[] states = US_States.getStates();
-                mapData = aggregateByState(entries, states);
-                break;
-            case "grid":
-                mapData = aggregateByGrid(entries, DEFAULT_GRID_SIZE);
-                break;
-        }
+    public Analyzer(MapAggregation aggregationFormat) {
+        this.aggregationFormat = aggregationFormat;
     }
 
-    private String aggregateByState(ArrayList<Record> entries, String[] states) {
+    public Analyzer(){
+        this(MapAggregation.US_State);
+    }
 
+    /* process list of data entries pre-filtered by parser into data structure
+       usable by map plotter */
+    public void processData(ArrayList<Record> entries) {
+        MapAggregation format = this.aggregationFormat;
+        String[] newEntries;
+
+        switch(format){
+            case US_State:
+                // Get list of us states with codes
+                String[] states = WorldMapData.US_STATES;
+                newEntries = aggregateByState(entries, states);
+                break;
+            case Grid:
+                newEntries = aggregateByGrid(entries, DEFAULT_GRID_SIZE);
+                break;
+            default:
+                newEntries = new String[];
+        }
+
+        this.mapData.addAll(Arrays.asList(newEntries));
+    }
+
+    private String[] aggregateByState(ArrayList<Record> entries, String[] states) {
         int stateCount = states.length;
         String[] dataByState = new String[stateCount];
 
@@ -33,16 +53,16 @@ public class Analyzer {
         for (int i = 0; i < stateCount; i++ ) {
             dataString = states[i] + ";";
             for (String field : this.FIELDS_OF_INTEREST ) {
+
                 dataString += field;
             }
             dataByState[i] = dataString;
         }
 
-        return "";
+        return new String[] {};
     }
 
-    private String aggregateByGrid(ArrayList<Record> entries, int gridSize) {
-        return "";
+    private String[] aggregateByGrid(ArrayList<Record> entries, int gridSize) {
+        return new String[] {};
     }
-
 }
--- bigdata/src/bigdata/WorldMap/MapAggregation.java
+++ bigdata/src/bigdata/WorldMap/MapAggregation.java
@@ -1,6 +1,7 @@
 package bigdata.WorldMap;
 
 public enum MapAggregation {
+    None,
     US_State,
     Grid
 }
